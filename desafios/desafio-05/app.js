8eb1764e686f3a8aa20d9c9037755327ce288c20
anbreaker <antunez19@gmail.com>
2021-07-22 13:03:19 +0200

    listas

---
+++ desafios/desafio-05/app.js
@@ -0,0 +1,21 @@
+new Vue({
+  el: '#desafio',
+
+  data: {
+    alternar: false,
+
+    nomes: ['Magalí', 'Bia', 'Ana', 'Rebeca'],
+
+    livro: {
+      titulo: 'O Senhor dos Anéis',
+      autor: 'J.R.R. Tolkiens',
+      volume: '3',
+    },
+
+    aluno: {
+      id: 10,
+      nome: 'Maria',
+      notas: [7.67, 8.33, 6.98, 9.21],
+    },
+  },
+});

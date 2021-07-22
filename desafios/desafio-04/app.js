b2b5257cf733490b62696e7abf91e327924a2416
anbreaker <antunez19@gmail.com>
2021-07-22 09:42:36 +0200

    dinamic styles

--- desafios/desafio-04/app.js
+++ desafios/desafio-04/app.js
@@ -1,10 +1,42 @@
 new Vue({
   el: '#desafio',
 
-  data: {},
-  methods: {
-    iniciarEfeito() {},
+  data: {
+    classe1: 'destaque',
+    perigo: false,
+    classe3: '',
+    classe4: '',
+    cor5: '',
+    estilo5: {
+      width: '50px',
+      height: '20px',
+    },
+    width: '0',
+  },
 
-    iniciarProgresso() {},
+  methods: {
+    iniciarEfeito() {
+      setInterval(() => {
+        this.classe1 = this.classe1 === 'destaque' ? 'encolher' : 'destaque';
+      }, 1000);
+    },
+
+    iniciarProgresso() {
+      console.log('entra');
+
+      let valor = 0;
+      const temporizador = setInterval(() => {
+        valor += 5;
+        this.width = `${valor}%`;
+        console.log(this.width);
+
+        if (valor === 100) clearInterval(temporizador);
+      }, 50);
+    },
+
+    setPerigo({ target }) {
+      if (target.value === 'true') this.perigo = true;
+      else this.perigo = false;
+    },
   },
 });

5d994ddd04241cc7eaa9d7c2f78d4f22ac44a0b2
anbreaker <antunez19@gmail.com>
2021-07-26 12:36:44 +0200

    slots vuejs

---
+++ componentes/citacoes/src/components/Citacao.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="citacao">
+    <slot name="autor"></slot>
+    <slot name="texto"></slot>
+    <div class="fonte">
+      <slot name="fonte"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  created() {
+    console.log('created');
+  },
+
+  destroyed() {
+    console.log('destroyed');
+  },
+
+  activated() {
+    console.log('activated');
+  },
+
+  deactivated() {
+    console.log('deactivated');
+  },
+};
+</script>
+
+<style scoped>
+.citacao {
+  border: 1px solid #ddd;
+  background-color: rgba(0, 0, 0, 0.1);
+  padding: 25px;
+  max-width: 450px;
+}
+
+.fonte {
+  float: right;
+}
+</style>

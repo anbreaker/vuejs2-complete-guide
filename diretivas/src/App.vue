7778a72474618d749daf34f26b98a86c72a52591
anbreaker <antunez19@gmail.com>
2021-07-29 13:32:21 +0200

    trabajando con diretivas

--- diretivas/src/App.vue
+++ diretivas/src/App.vue
@@ -7,8 +7,12 @@
     <p v-destaque="cor">Usando Custom Diretiva</p>
 
     <hr />
+    <p v-destaque-local-pisca.alternar="'blue'">Usando Custom Diretiva Local Pisca</p>
+    <p v-destaque-local-pisca:fundo.atrasar.alternar>Usando diretiva personalizada</p>
+    <hr />
+
     <!-- <p v-nome:argumento.modificador1,modificador2="'valor'"></p> -->
-    <p v-destaque-local="'yellow'">Usando Custom Diretiva Local</p>
+    <p v-destaque-local-simple="'yellow'">Usando Custom Diretiva Local</p>
     <p v-destaque:fundo="'yellow'">Usando Custom Diretiva, Argumento</p>
     <p v-destaque="'DodgerBlue'">Usando Custom Diretiva</p>
     <p v-destaque:fundo.atrasar="'#336699'">Usando Custom Diretiva, Argumento</p>
@@ -18,9 +22,38 @@
 <script>
 export default {
   directives: {
-    "destaque-local": {
+    "destaque-local-simple": {
       bind(el) {
         el.style.backgroundColor = "orange"
+      },
+    },
+    "destaque-local-pisca": {
+      bind(el, binding) {
+        const aplicarCor = (cor) => {
+          if (binding.arg === "fundo") {
+            el.style.backgroundColor = cor
+          } else {
+            el.style.color = cor
+          }
+        }
+
+        let atraso = 0
+        if (binding.modifiers["atrasar"]) atraso = binding.value.atraso
+
+        const cor1 = binding.value.cor1
+        const cor2 = binding.value.cor2
+        let corAtual = cor1
+
+        setTimeout(() => {
+          if (binding.modifiers["alternar"]) {
+            setInterval(() => {
+              corAtual = corAtual === cor1 ? cor2 : cor1
+              aplicarCor(corAtual)
+            }, binding.value.intervalo)
+          } else {
+            aplicarCor(binding.value)
+          }
+        }, atraso)
       },
     },
   },

2f9bdac173cd836354c01cfb88e7fcced33a0c25
anbreaker <antunez19@gmail.com>
2021-08-10 10:43:00 +0200

    trabajando con props

--- vuex-exercicios/stock-trader/src/components/Stocks/Stock.vue
+++ vuex-exercicios/stock-trader/src/components/Stocks/Stock.vue
@@ -1,12 +1,56 @@
 <template>
-  <div>
-    <h1>Ação</h1>
-  </div>
+  <v-flex class="pr-3 pb-3" xs12 md6 lg4>
+    <v-card class="green darken-3 white--text">
+      <v-card class="headline green darken-3 white--text">
+        <strong>
+          {{ stockProp.name }} <small>(Preço: {{ stockProp.price }})</small>
+        </strong>
+      </v-card>
+    </v-card>
+    <v-card>
+      <v-container fill-height>
+        <v-text-field
+          type="number"
+          label="Quantidade"
+          v-model.number="quantity"
+        />
+        <v-btn
+          class="green darken-3 white--text"
+          :disabled="quantity <= 0 || !Number.isInteger(quantity)"
+          @click="buyStock"
+          >Comprar
+        </v-btn>
+      </v-container>
+    </v-card>
+  </v-flex>
 </template>
 
 <script>
-export default {};
+export default {
+  props: ["stockProp"],
+
+  data() {
+    return {
+      quantity: 0
+    };
+  },
+
+  methods: {
+    buyStock() {
+      const order = {
+        stockId: this.stockProp.id,
+        stockPrice: this.stockProp.price,
+        quantity: this.quantity
+      };
+      console.log(order);
+      this.quantity = 0;
+    }
+  }
+};
 </script>
 
 <style scoped>
+strong {
+  margin-left: 8px;
+}
 </style>
--- vuex-exercicios/stock-trader/src/components/Stocks/Stocks.vue
+++ vuex-exercicios/stock-trader/src/components/Stocks/Stocks.vue
@@ -1,11 +1,28 @@
 <template>
-  <div>
-    <h1>Ações</h1>
-  </div>
+  <v-layout row wrap>
+    <Stock v-for="stock in stocks" :key="stock.id" :stockProp="stock"> </Stock>
+  </v-layout>
 </template>
 
 <script>
-export default {};
+import Stock from "./Stock.vue";
+
+export default {
+  components: {
+    Stock
+  },
+
+  data() {
+    return {
+      stocks: [
+        { id: 1, name: "BMW", price: 110 },
+        { id: 2, name: "Google", price: 200 },
+        { id: 3, name: "Apple", price: 280 },
+        { id: 4, name: "Twitter", price: 83 }
+      ]
+    };
+  }
+};
 </script>
 
 <style scoped>

93307d55cf37efe209b2cd143d8aaf777e19b721
anbreaker <antunez19@gmail.com>
2021-08-16 09:48:21 +0200

    components y css

---
+++ desafios/desafio-13-problema-monty-hall/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <h1>Problema Monty Hanll</h1>
+    <Door number="1" hasGift="false" />
+  </div>
+</template>
+
+<script>
+import Door from "./components/Door.vue";
+
+export default {
+  name: "App",
+
+  components: {
+    Door
+  }
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  font-family: "Courier New", Courier, monospace, sans-serif;
+}
+
+body {
+  color: azure;
+  background: linear-gradient(62deg, #fbab7e 0%, #f7ce68 100%);
+}
+
+#app {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+#app h1 {
+  border: 1px solid #000;
+  background-color: #0004;
+  padding: 20px;
+  margin-bottom: 60px;
+}
+</style>

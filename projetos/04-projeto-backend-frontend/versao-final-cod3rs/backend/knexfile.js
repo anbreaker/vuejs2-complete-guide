93e35b894316806c11130418f318b7407e16f899
anbreaker <antunez19@gmail.com>
2021-08-16 14:33:01 +0200

    iniciando backend...

--- projetos/04-projeto-backend-frontend/versao-final-cod3rs/backend/knexfile.js
+++ projetos/04-projeto-backend-frontend/versao-final-cod3rs/backend/knexfile.js
@@ -1,13 +1,13 @@
-const { db } = require('./.env')
+const { db } = require("./.env.database")
 
 module.exports = {
-	client: 'postgresql',
-	connection: db,
-	pool: {
-		min: 2,
-		max: 10
-	},
-	migrations: {
-		tableName: 'knex_migrations'
-	}
-};
+  client: "postgresql",
+  connection: db,
+  pool: {
+    min: 2,
+    max: 10,
+  },
+  migrations: {
+    tableName: "knex_migrations",
+  },
+}
